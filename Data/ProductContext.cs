d50f252a07ee0107f2a284354add156aed9ef6d6
Steven Duncan <Steven.Duncan@paccar.com>
2023-08-10 14:24:11 -0700

    Adding Category table.

--- Data/ProductContext.cs
+++ Data/ProductContext.cs
@@ -5,13 +5,14 @@ namespace CarvedRock.Admin.Data;
 public class ProductContext : DbContext
 {
   public DbSet<Product> Products => Set<Product>();
+  public DbSet<Category> Categories => Set<Category>();
 
   public string DbPath { get; set; }
 
-  public ProductContext()
+  public ProductContext(IConfiguration config)
   {
     var path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    DbPath = Path.Join(path, "carved-rock.db");
+    DbPath = Path.Join(path, config.GetConnectionString("ProductDbFileName"));
   }
 
   protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
@@ -24,13 +25,23 @@ public class ProductContext : DbContext
       SaveChanges();
     }
 
+    if (Categories.Any())
+    {
+      Categories.RemoveRange(Categories);
+      SaveChanges();
+    }
+
+    var footwearCat = new Category { Name = "Footwear", Id = 1000 };
+    var equipmentCat = new Category { Name = "Equipment", Id = 2000 };
+
     Products.Add(new Product
     {
       Id = 1,
       Name = "Trailblazer",
       Price = 69.99M,
       IsActive = true,
-      Description = "Great support in this high-top to take you to great heights and trails."
+      Description = "Great support in this high-top to take you to great heights and trails.",
+      Category = footwearCat
     });
     Products.Add(new Product
     {
@@ -38,8 +49,8 @@ public class ProductContext : DbContext
       Name = "Coastliner",
       Price = 49.99M,
       IsActive = true,
-      Description =
-            "Easy in and out with this lightweight but rugged shoe with great ventilation to get your around shores, beaches, and boats."
+      Description = "Easy in and out with this lightweight but rugged shoe with great ventilation to get your around shores, beaches, and boats.",
+      Category = footwearCat
     });
     Products.Add(new Product
     {
@@ -47,8 +58,8 @@ public class ProductContext : DbContext
       Name = "Woodsman",
       Price = 64.99M,
       IsActive = true,
-      Description =
-            "All the insulation and support you need when wandering the rugged trails of the woods and backcountry."
+      Description = "All the insulation and support you need when wandering the rugged trails of the woods and backcountry.",
+      Category = footwearCat
     });
     Products.Add(new Product
     {
@@ -56,8 +67,10 @@ public class ProductContext : DbContext
       Name = "Basecamp",
       Price = 249.99M,
       IsActive = true,
-      Description = "Great insulation and plenty of room for 2 in this spacious but highly-portable tent."
+      Description = "Great insulation and plenty of room for 2 in this spacious but highly-portable tent.",
+      Category = equipmentCat
     });
+    Categories.AddRange(new Category[] { footwearCat, equipmentCat });
 
     SaveChanges();
   }
---
+++ Models/CategoryModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace CarvedRock.Admin.Models;
+
+public class CategoryModel
+{
+  public int Id { get; set; }
+  [Required]  
+  public string Name { get; set; } 
+}
